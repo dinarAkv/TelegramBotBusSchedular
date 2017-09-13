464e092cbe564770bc18a78fd26dfe80de2d945e
ITECO\akhmetzyanov <akhmetzyanov@i-teco.ru>
2017-09-13 17:41:40 +0300

    Begin to adding logger aop.

---
+++ src/main/java/ru/cheb/intercity/bus/logger/ParsersLoggerAspect.java
@@ -0,0 +1,32 @@
+package ru.cheb.intercity.bus.logger;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import ru.cheb.intercity.bus.parsers.BusStationSchedulerParser;
+
+@Aspect
+@Component
+public class ParsersLoggerAspect {
+
+
+    final static Logger logger = Logger.getLogger(ParsersLoggerAspect.class);
+
+    @Around("execution(ru.cheb.intercity.bus.parsers.BusStationSchedulerParser get*())")
+    public Object getSchedulerLog(ProceedingJoinPoint joinPoint){
+        try {
+            logger.info("HELLO AOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPPPPPPPPPPPPPPPPPP");
+
+            Object proceed = joinPoint.proceed();
+            return proceed;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        throw new IllegalStateException();
+    }
+
+
+}
