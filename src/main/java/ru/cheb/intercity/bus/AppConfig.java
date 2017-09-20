2961ca4f03d87a37531fda41a631e87a93f4ee60
ITECO\akhmetzyanov <akhmetzyanov@i-teco.ru>
2017-09-20 16:27:10 +0300

    Change default spring boot icon to custom icon.

---
+++ src/main/java/ru/cheb/intercity/bus/AppConfig.java
@@ -0,0 +1,12 @@
+package ru.cheb.intercity.bus;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+
+@Configuration
+public class AppConfig {
+
+
+
+}
